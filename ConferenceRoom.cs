bad1a5269f772aea2216b5ca6c64306cc17cdb6a
TJ Gaba <tjgaba@outlook.com>
2026-01-27 00:57:46 +0200

    Create ConferenceRoom.cs logic/rule

---
+++ ConferenceRoom.cs
@@ -0,0 +1,28 @@
+namespace ConferenceRoomBooking
+{
+    public class ConferenceRoom
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Capacity { get; }
+        public RoomAvailability Availability { get; private set; }
+
+            public ConferenceRoom(int id, string name, int capacity, RoomAvailability availability)
+            {
+                Id = id;
+                Name = name;
+                Capacity = capacity;
+                Availability = availability;
+            }
+
+        public void MarkUnavailable()
+        {
+            Availability = RoomAvailability.Unavailable;
+        }
+
+        public void MarkAvailable()
+        {
+            Availability = RoomAvailability.Available;
+        }
+    }
+}
