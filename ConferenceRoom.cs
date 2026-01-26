1f9a5239c7ce4e03d6ad0b0fabd1e35d04ae96e4
tjgaba <48362508+tjgaba@users.noreply.github.com>
2026-01-27 01:59:50 +0200

    Assignment Final Review Edit - Restore Code

--- ConferenceRoom.cs
+++ ConferenceRoom.cs
@@ -15,6 +15,16 @@ namespace ConferenceRoomBooking
                 Availability = availability;
             }
 
+            public void MarkUnavailable()
+            {
+                Availability = RoomAvailability.Unavailable;
+            }
+
+            public void MarkAvailable()
+            {
+                Availability = RoomAvailability.Available;
+            }
+
         
     }
 }
